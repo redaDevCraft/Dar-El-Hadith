eab7f2cbaffa18473ed491fc36883cd09b2ce266
taleb-boop <tlbreda8@gmail.com>
2024-10-05 14:38:19 +0100

    Optimize hero section background image in manifest.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        news::factory(5)->create();
+        
 
         User::factory()->create([
             'name' => 'Test User',
